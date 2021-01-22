bae0b7fdb706b4767fc34ae2cd84677402f53461
u-aiaa <uj.akiyama@gmail.com>
2021-01-22 18:46:04 +0900

    Add

--- main.go
+++ main.go
@@ -1,13 +1,87 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+)
+
+// Task struct is task's info
+type Task struct {
+	ID      string `json:"id" form:"id" binding:"-"`
+	Content string `json:"content" form:"content" binding:"required"`
+	Done    bool   `json:"done" form:"done"`
+}
+
+var tasks = make(map[string]Task, 0)
 
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(200, gin.H{
-      "message": "pong",
-    })
-  })
-  r.Run()
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/tasks", func(c *gin.Context) {
+		list := make([]Task, 0, len(tasks))
+		for _, task := range tasks {
+			list = append(list, task)
+		}
+		c.JSON(http.StatusOK, list)
+	})
+
+	r.GET("/task/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		task, ok := tasks[id]
+		if ok {
+			c.JSON(http.StatusOK, task)
+		} else {
+			c.Status(http.StatusNotFound)
+		}
+	})
+
+	r.POST("/task", func(c *gin.Context) {
+		var task Task
+		if err := c.ShouldBind(&task); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			guid := xid.New()
+			task.ID = guid.String()
+			tasks[task.ID] = task
+			c.JSON(http.StatusOK, task)
+		}
+	})
+
+	r.PUT("/task/:id", func(c *gin.Context) {
+		var task Task
+		if err := c.ShouldBind(&task); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			id := c.Param("id")
+			_, ok := tasks[id]
+			if ok {
+				task.ID = id
+				tasks[id] = task
+				c.JSON(http.StatusOK, task)
+			} else {
+				c.Status(http.StatusNotFound)
+			}
+		}
+	})
+
+	r.DELETE("/task/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		task, ok := tasks[id]
+		if ok {
+			delete(tasks, id)
+			c.JSON(http.StatusOK, task)
+		} else {
+			c.Status(http.StatusNotFound)
+		}
+	})
+
+	r.Run()
 }
